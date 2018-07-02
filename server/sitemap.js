0e209bd439000dc911e0138bd88f26fce2fa404f
Jiang Fengming <jiangfengming87@gmail.com>
2018-07-02 20:46:50 +0800

    sitemap.count: limit query; change response field names

--- server/sitemap.js
+++ server/sitemap.js
@@ -1,5 +1,5 @@
 const { db } = require('../shared/db')
-const sitemap = db.collection('sitemaps')
+const sitemaps = db.collection('sitemaps')
 const { URL } = require('url')
 const CustomError = require('../shared/CustomError')
 
@@ -8,15 +8,27 @@ const PAGE_SIZE = 50000
 module.exports = {
   async count(ctx) {
     const site = ctx.params.site
+    let limit
+    if (ctx.query.limit) {
+      limit = parseInt(ctx.query.limit)
+      if (isNaN(limit) || limit <= 0 || limit > PAGE_SIZE) throw new CustomError('CLIENT_INVALID_PARAM', 'limit')
+    } else {
+      limit = PAGE_SIZE
+    }
+
     try {
       new URL(site)
     } catch (e) {
       throw new CustomError('CLIENT_INVALID_PARAM', 'site')
     }
 
-    const urlCount = await sitemap.count({ site })
-    const sitemapCount = Math.ceil(urlCount / PAGE_SIZE)
+    const urlCount = await sitemaps.count({ site })
+    const sitemapCount = Math.ceil(urlCount / limit)
     const sitemapIndexCount = Math.ceil(sitemapCount / PAGE_SIZE)
-    ctx.body = { urlCount, sitemapCount, sitemapIndexCount }
+    ctx.body = {
+      url: urlCount,
+      sitemap: sitemapCount,
+      sitemapIndex: sitemapIndexCount
+    }
   }
 }
