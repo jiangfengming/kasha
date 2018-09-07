4571b560d3fe7b1585ecaade5212b24a8001118f
Jiang Fengming <jiangfengming87@gmail.com>
2018-09-07 10:41:08 +0800

    add separate bin files

--- server/index.js
+++ server/index.js
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 (async() => {
   await require('../install')
 
--- worker/index.js
+++ worker/index.js
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 (async function() {
   const { URL } = require('url')
   const RESTError = require('../shared/RESTError')
