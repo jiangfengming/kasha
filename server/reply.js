e7dd85478a9232728465c216d26380f095caa3a1
Jiang Fengming <jiangfengming87@gmail.com>
2019-04-12 04:09:43 +0800

    add back `Cache-Control: max-age` headerr

--- server/reply.js
+++ server/reply.js
@@ -4,14 +4,16 @@ function reply(ctx, type, followRedirect, doc, cacheStatus) {
   const { updatedAt } = doc
   let { privateExpires } = doc
 
-  const age = Math.round((Date.now() - updatedAt) / 1000)
-
   if (privateExpires < Date.now()) {
     privateExpires = new Date(Date.now() + maxStale * 1000)
   }
 
+  const age = Math.round((Date.now() - updatedAt) / 1000)
+  let maxage = Math.round((privateExpires - Date.now()) / 1000)
+
   ctx.set('Age', age)
   ctx.set('Last-Modified', updatedAt.toUTCString())
+  ctx.set('Cache-Control', `max-age=${maxage}`)
   ctx.set('Expires', privateExpires.toUTCString())
   ctx.set('Kasha-Code', 'OK')
   ctx.set('kasha-Cache-Status', cacheStatus)
